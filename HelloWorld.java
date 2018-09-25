f7b0559b59853165d9f355710ca24ce128dc082c
paranoia1906 <anthonymledesma@gmail.com>
2018-09-25 10:06:32 -0700

    Added function that works with strings and integers to do some basic math and console printing.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        
+
         System.out.println("Hello World!");
     }
 }
 
 public class PrintFavoriteNumber {
     public static void main(String args[]) {
-        
+
         double favoriteNumber = 28.8;
         System.out.println("My favorite number is " + favoriteNumber);
     }
@@ -15,15 +15,23 @@ public class PrintFavoriteNumber {
 
 public class BooleanExampleClass {
     public static void main(String args[]) {
-        
+
         boolean amIMale = true;
         boolean amISleepy = true;
         String finalString = "";
-        if(amIMale == true && amISleepy == true){
+        if (amIMale == true && amISleepy == true) {
             finalString = "I am one sleepy man!";
         } else {
             finalString = "I am a man... But I am not sleepy!";
         }
         System.out.println(finalString);
+    }
 }
+
+public class WorkingWithStrings {
+    public static void main(String args[]) {
+
+        String myString = "asdawdawdaw2123123";
+        System.out.println(myString.length());
+    }
 }
