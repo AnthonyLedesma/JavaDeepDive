26ce5abcb81e83b6f808359163e57d65852f953f
paranoia1906 <anthonymledesma@gmail.com>
2018-09-30 11:55:37 -0700

    Image fade from bart to homer.

--- FadeAni/app/src/main/java/com/app/aledesma/fadinganimations/MainActivity.java
+++ FadeAni/app/src/main/java/com/app/aledesma/fadinganimations/MainActivity.java
@@ -2,8 +2,21 @@ package com.app.aledesma.fadinganimations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view){
+        Log.i("Info","Image View tapped");
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.animate().alpha(0).setDuration(2000);
+
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView3);
+        imageView2.animate().alpha(1).setDuration(2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
