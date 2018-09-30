38e95295df1168cd6964adbc145766e91630c250
paranoia1906 <anthonymledesma@gmail.com>
2018-09-30 12:06:03 -0700

    Animation will fade from one image to another and back again with image clicks.

--- FadeAni/app/src/main/java/com/app/aledesma/fadinganimations/MainActivity.java
+++ FadeAni/app/src/main/java/com/app/aledesma/fadinganimations/MainActivity.java
@@ -9,13 +9,20 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     public void fade(View view){
-        Log.i("Info","Image View tapped");
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-        imageView.animate().alpha(0).setDuration(2000);
 
-        ImageView imageView2 = (ImageView) findViewById(R.id.imageView3);
-        imageView2.animate().alpha(1).setDuration(2000);
+        ImageView imageViewBart = (ImageView) findViewById(R.id.imageView2);
+        ImageView imageViewHomer = (ImageView) findViewById(R.id.imageView3);
+        String out = String.valueOf(imageViewBart.getAlpha());
+
+        if(imageViewBart.getAlpha() == 1.0){
+            imageViewBart.animate().alpha(0).setDuration(2000);
+            imageViewHomer.animate().alpha(1).setDuration(2000);
+        } else {
+            imageViewBart.animate().alpha(1).setDuration(2000);
+            imageViewHomer.animate().alpha(0).setDuration(2000);
+        }
+        Log.i("Info", out);
     }
 
     @Override
